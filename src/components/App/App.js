473100396ce3e68d8be5ee69c08e69d7b2c1d98f
Hugo Georget <hugo.georget@sword-group.com>
2019-11-26 11:59:48 +0100

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import News from "../News/News";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <News></News>
+    </div>
+  );
+}
+
+export default App;
