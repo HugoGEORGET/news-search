9541a996639ac6410a8b589be26860c24f0907e3
Hugo Georget <hugo.georget@sword-group.com>
2019-11-26 14:33:40 +0100

    Added linting dependencies

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,7 +5,9 @@ const News = () => {
   const [newsResults, setNewsResults] = useState({});
 
   async function fetchNews() {
-    const result = await fetch("https://newsapi.org/v2/everything?q=test&sortBy=popularity&apiKey=");
+    const result = await fetch(
+      "https://newsapi.org/v2/everything?q=test&sortBy=popularity&apiKey="
+    );
     result
       .json()
       .then(result => setNewsResults(result))
