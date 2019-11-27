9c942cd2e0e3f810dc5b5478150ab2fa493b696d
Hugo Georget <hugo.georget@sword-group.com>
2019-11-27 15:26:49 +0100

    Updates

---
+++ precache-manifest.1055ccff5ed2c9579c574f4488c44ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899b61b3314b57637da3f68298195d9e",
+    "url": "/news-search/index.html"
+  },
+  {
+    "revision": "e4d94f033326e15117c6",
+    "url": "/news-search/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "99f857e896788eb82cc6",
+    "url": "/news-search/static/css/main.4c95cfdf.chunk.css"
+  },
+  {
+    "revision": "e4d94f033326e15117c6",
+    "url": "/news-search/static/js/2.e1739fc4.chunk.js"
+  },
+  {
+    "revision": "99f857e896788eb82cc6",
+    "url": "/news-search/static/js/main.98a207fe.chunk.js"
+  },
+  {
+    "revision": "c8935fc8a98662735464",
+    "url": "/news-search/static/js/runtime-main.9ed73278.js"
+  }
+]);
